1b0ba2a917803b2fcef35fd43088f3c12cc620c2
Said Ali <saidibnali19@gmail.com>
2025-02-05 09:48:15 +0300

    style a button, its hover, focus and active states using Tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold underline">Hello, world!</h1>
+      <h1 className="font-bold">Hello, world!</h1>
+      <button className="hover:bg-sky-500 focus:outline-sky-500  active:bg-sky-400">Save changes</button>
+      <button className="bg-lime-900 text-slate-50 hover:bg-lime-700 focus:outline-lime-700 focus:outline-offset-2 active:bg-lime-600">Learn more</button>
     </>
   );
 }
